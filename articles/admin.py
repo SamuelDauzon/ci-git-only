62ea14f9a7d0597654811a92eb9fcd31a20655e8
Dauzon <git@dauzon.com>
2019-01-27 21:33:01 +0100

    Users & Articles : setup admin interface

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from .models import Article
+
+class ArticleAdmin(ModelAdmin):
+    search_fields = ('title', )
+    list_display = ('author', 'title', 'is_display', 'is_index', )
+    list_filter = ('is_display', 'is_index', )
+    list_editable = ('is_display', 'is_index', )
+
+admin.site.register(Article, ArticleAdmin)
