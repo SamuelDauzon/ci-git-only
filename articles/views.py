be6a8dfe5eebed3c84a5197dc372527ff821afcb
Dauzon <git@dauzon.com>
2019-01-30 23:04:55 +0100

    Article : list page with pagination

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,21 @@
+# coding:utf-8
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# Create your views here.
+from .models import Article
+
+def list_articles(request):
+    all_articles = Article.objects.filter(is_display=True, is_index=True)
+    paginator = Paginator(all_articles, 10)
+    page = request.GET.get('page')
+    try:
+        articles = paginator.get_page(page)
+    except PageNotAnInteger:
+        articles = paginator.get_page(1)
+    except EmptyPage:
+        articles = paginator.get_page(paginator.num_pages)
+    return render(
+        request, 
+        'articles/list.html', 
+        {'articles' : articles, }
+        )
