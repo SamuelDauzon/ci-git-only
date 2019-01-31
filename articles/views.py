ce041930fd1eeca073cde0cef1732e9fd409505f
Dauzon <git@dauzon.com>
2019-01-31 21:47:10 +0100

    Article : detail page

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 # coding:utf-8
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Article
@@ -18,4 +18,12 @@ def list_articles(request):
         request, 
         'articles/list.html', 
         {'articles' : articles, }
+        )
+
+def detail(request, slug, id):
+    article = get_object_or_404(Article, id=id, is_display=True)
+    return render(
+        request, 
+        'articles/detail.html', 
+        {'article' : article, }
         )
